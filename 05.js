789ee824d3ff8b3c6e8fd1ebfdfe5e020517f25d
miguelsanchez <sanchezmiguel7@gmail.com>
2023-02-05 17:44:16 -0400

    resolucion ejercicio5

--- 05.js
+++ 05.js
@@ -4,7 +4,7 @@
   código dentro de las funciones ya definidas. 
   No comentar la funcion 
 */
-function pluck(array, propiedad) {
+function pluck(array, name) {
   // La función llamada 'pluck' recibe como argumento un array de objetos llamado 'array' y el nombre de una
   // propiedad.
   // La función debe devolver un nuevo arreglo con solo los valores dentro de la propiedad recibida
@@ -14,8 +14,24 @@ function pluck(array, propiedad) {
   // Pista: es una buena oportunidad para usar map.
 
   // Tu código acá:
+  var nuevoArreglo = array.map((pro) => {
+        console.log(pro.name);
+        return pro;
+  });
+  
+
+  
+  /* for(i = 0; i < array.length; i++){
+        if (array[i].name == name){
+              return array[i];
+        } 
+  } */
 
 }
+
+var array = [{ name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 }];
+pluck(array,'name');
+
 
 // No modifiques nada debajo de esta linea //
 
